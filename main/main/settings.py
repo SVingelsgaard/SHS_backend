d2c05569aca809946bbdbf9c2e46594847b58e37
SVingelsgaard <sigurd@vingelsgaard.no>
2023-05-21 15:30:57 +0200

    database?

--- main/main/settings.py
+++ main/main/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
+import dj_database_url#for vercel database
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,14 +84,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'main.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# Database. (Vercel postgres)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        'postgres://default:I6wFvEuOLjP1@ep-black-mud-482295.eu-central-1.postgres.vercel-storage.com:5432/verceldb'
+    )
 }
 
 
