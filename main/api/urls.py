91565e90670eefed275ed6be93955faefc72390d
SVingelsgaard <sigurd@vingelsgaard.no>
2023-10-14 20:08:18 +0200

    adde zero or one endpoint

--- main/api/urls.py
+++ main/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('bustimes/', views.getBustimes),
+    path('one_or_zero/', views.one_or_zero, name='one_or_zero'),
     path('', views.apiHomescreen, name='apiHomescreen'),
 ]
--- main/api/views.py
+++ main/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .scraper import run_scrape
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 def apiHomescreen(request):
     return render(request, "api/templates/api_homescreen.html")
@@ -20,3 +21,9 @@ def getBustimes(request):
         return Response(serializer.data, status=201)
     else:
         return Response(serializer.errors, status=400)
+    
+def one_or_zero(request):
+    # You can define your logic to determine the value (1 or 0) here.
+    value = 1  # Replace with your logic
+
+    return JsonResponse({'result': value})
