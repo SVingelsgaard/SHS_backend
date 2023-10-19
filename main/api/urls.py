75bb8ccb3c67444c964c55658c6c42583f272d77
SVingelsgaard <sigurd@vingelsgaard.no>
2023-10-19 22:48:06 +0200

    removed button thing

--- main/api/urls.py
+++ main/api/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [ 
     path('bustimes/', views.getBustimes),
-    path('openDoorButton/', views.openDoorButton, name='openDoorButton'),
     path('openDoor/', views.openDoorEP, name='openDoorEP'),
     path('', views.apiHomescreen, name='apiHomescreen'),
 ]
